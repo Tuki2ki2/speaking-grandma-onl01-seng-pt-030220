fea320cd5eacfcdd005382e334ed2817f178141d
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-02 22:08:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@ require "pry"
 phrase = "whatever"
 def speak_to_grandma(phrase)
   if phrase !== phrase.uppercase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase ==phrase.uppercase
-    puts "NO, NOT SINCE 1938!"
+    return  "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   end
   binding.pry
 end
