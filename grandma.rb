d53b8079ff21f487346f9ee705f54fd30a481c6d
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-02 22:07:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ phrase = "whatever"
 def speak_to_grandma(phrase)
   if phrase !=="#{phrase}.uppercase"
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase ==phrase.uppercase 
+  elsif phrase ==phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
